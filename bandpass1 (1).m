a0166f8be91a14daa685a6c2a1c8c4dd08499dd3
joeljijo <161542510+joeljijo@users.noreply.github.com>
2024-03-04 01:01:58 -0800

    Update bandpass1 (1).m

--- bandpass1 (1).m
+++ bandpass1 (1).m
@@ -1,7 +1,16 @@
 function bandpass1(a,b,filename)
+%takes the range of frequencies a and b as well as the name of the wave
+%file
 [x, fs]=audioread(filename);
+%gets fs from the file as well as x, which is the column vector with the
+%audiodaata
 n = length(x);
+%gets the length which might be useful
 xf=bandpass(x,[a b],fs);
+%applies the bandpass filter
 plot(x); hold on;
 plot(xf);
+%plots the filter
 sound(xf,fs)
+%plays the sound
+end
