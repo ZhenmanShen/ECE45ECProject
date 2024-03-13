8c849edaee2d92369971e43ed596487b85333121
JacobZhang058 <jacobzhang026@gmail.com>
2024-03-12 22:20:36 -0700

    Fixed the Freq. Res. Graphs
    -Hotfix for freq res graphs
    -Changed the UI to fit the bigger graphs, but it looks ugly rn so please feel free to continue to edit it
    -I don't know why github insists that the file name of plot_freqResSin.m is actually plot_FreqResSin.m, but maybe that's just on my end.

--- plot_FreqResSin.m
+++ plot_FreqResSin.m
@@ -1,7 +1,7 @@
-function plot_freqResSin(samplingRate, frequency, filteredSignal)
+function plot_freqResSin(samplingRate, frequency, filteredSignal, graphMag, graphPhase)
     %Time length set to 1
     T = 1;
     t = linspace(0, T, T*samplingRate);
     origSignal = sin(2 * pi * frequency * t);
-    plot_freqRes(origSignal, filteredSignal, samplingRate);
+    plot_freqRes(origSignal, filteredSignal, samplingRate, graphMag, graphPhase);
 end
--- plot_freqRes.m
+++ plot_freqRes.m
@@ -1,14 +1,17 @@
-function plot_freqRes(origSignal, filteredSignal, fs)
+function plot_freqRes(origSignal, filteredSignal, fs, graphMag, graphPhase)
     % X is the input signal, Y is the output signal
     % H is the frequency response
-    % Sampling Frequency set at 44100
     X = fft(origSignal);
     Y = fft(filteredSignal);
+
+    dimensions = size(Y);
+    X = resize(X, dimensions);
+
     H = Y ./ X;
 
     % Plot the magnitude and phase of the frequency response
     frequencies = linspace(0, fs/2, length(H));
     
-    plot(app.graph_FR_M, frequencies, abs(H));
-    plot(app.graph_FR_P, frequencies, angle(H));
+    plot(graphMag, frequencies, abs(H));
+    plot(graphPhase, frequencies, angle(H));
 end
