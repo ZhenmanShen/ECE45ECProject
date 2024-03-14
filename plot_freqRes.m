78b09e682908f0677a2c8966808569ab9410bc36
JacobZhang058 <jacobzhang026@gmail.com>
2024-03-13 17:56:49 -0700

    Added documentation

--- plot_freqRes.m
+++ plot_freqRes.m
@@ -1,3 +1,17 @@
+% plot_freqRes: plots the frequency response graphs for the applied filter
+
+% Contributor:
+% Jacob Zhang
+
+% Documentation:
+% origSignal - the original signal with no filter applied
+% filteredSignal - the filtered signal
+% fs - the frequency sampling rate
+% graphMag - the graph where the magnitude of the frequency response should
+% be applied
+% graphPhase - the graph where the phase of the frequency response should 
+% be applied
+
 function plot_freqRes(origSignal, filteredSignal, fs, graphMag, graphPhase)
     % X is the input signal, Y is the output signal
     % H is the frequency response
