2b1d832e2849d2c5e9612fd56665dfd46b3cece1
ethankook <93116378+ethankook@users.noreply.github.com>
2024-03-20 16:56:11 -0700

    fixed documentation

--- src/SpeedAdjuster.m
+++ src/SpeedAdjuster.m
@@ -1,8 +1,17 @@
 
-% plays speed-shifted audio when button is clicked
+% Speed Adjuster: Changes the speed and the pitch of the given audio
+
+% Contributors:
+% Ethan Kook
+% 
+
+% Documentation:
+% input_wave: the input audio file
+% shift_slider: the value of the slider that changes the speed of the audio
+
+
 function SpeedAdjuster(input_wave,shift_slider)
     [audio, Fs] = audioread(input_wave);
-
 
     freqChange = 2^(shift_slider.Value/12);
 % calculates percent change in frequency. 12 semitones. So if shift=2
@@ -10,6 +19,9 @@ function SpeedAdjuster(input_wave,shift_slider)
 % 11.25% increase in frequency
 
     output_wave = resample(audio, Fs, round(Fs * freqChange));
+
+%changing the sample rate of an audio causes the speed and the pitch to change. 
+
 
 end
 
