b450c2ebb41fc4b3a3aa8b41b2075179b9c6b089
projectsbykyle <114366271+projectsbykyle@users.noreply.github.com>
2024-03-22 16:20:58 -0700

    Update audio_fade_in.m

--- src/audio_fade_in.m
+++ src/audio_fade_in.m
@@ -12,12 +12,19 @@ function output = audio_fade_in(input, freq, fadeDuration)
     fade_in = linspace(0, 1, samples)'.^2;
 
     % Apply fade-in effect
-    output = input;   
     for i=1:samples
-        output(i) = output(i) * fade_in(i); 
+        if i < samples && i < length(input)
+            input(i) = input(i) * fade_in(i); 
+        end 
+    end 
+
+    %Copy the audio data to an output audio wave. 
+    output = zeros(length(input), 1); 
+    
+    for i=1:length(input)
+        output(i) = input(i); 
     end 
 
     % Write audio with fade-in to a new file
     sound(output, freq); 
-    %audiowrite(outputFilePath, yFadeIn, Fs);
 end
