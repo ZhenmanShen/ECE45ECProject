79213ae7109a8324c335cdbb5aaa9230ee2fb38e
projectsbykyle <114366271+projectsbykyle@users.noreply.github.com>
2024-03-22 16:20:41 -0700

    Update audio_fade_out.m

--- src/audio_fade_out.m
+++ src/audio_fade_out.m
@@ -1,5 +1,6 @@
 function output = audio_fade_out(input, freq, fadeDuration)
-    %Check User Input. If it is greater than the length of the audio, we set the fade duration to the length of the audio 
+    %Check User Input. If it is greater than the length of the audio, 
+    % we set the fade duration to the length of the audio 
     if fadeDuration > (length(input) / freq)
         fadeDuration = (length(input) / freq); 
     end
@@ -7,16 +8,24 @@ function output = audio_fade_out(input, freq, fadeDuration)
     % Convert fade duration to samples
     samples = fadeDuration * freq;
 
-    % Create fade-in vector. We fill it with quadratically spaced values from
+    % Create fade-out vector. We fill it with quadratically spaced values from
     % 0 to 1 to emphasize the fade-in effect. We then flip it for the fade
     % out effect. 
     fade_out = flip(linspace(0, 1, samples)'.^2);
     
+    % Apply fade-out effect. The k variable indexes the fade_out vector. 
+    k = 1;
+    for i = length(input)-samples+1:length(input)
+        input(i) = input(i)*fade_out(k); 
+        k=k+1; 
+    end 
+  
+    % Write audio with fade-out to a new file. 
+    output = zeros(length(input), 0); 
+    for i = 1:length(input)
+        output(i) = input(i);
+    end 
 
-    % Apply fade-out effect
-    output = input;
-    output(end-samples+1:end) = input(end-samples+1:end) .* fade_out;
-
-    % Write audio with fade-in to a new file
+    %Output audio for the user. 
     sound(output, freq); 
 end
